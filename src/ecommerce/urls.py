965a2b364c3158c66a25ab73eee622b3a6fb9d42
Robert Dunn <robertdunn220@gmail.com>
2018-10-18 12:11:35 -0400

    Changed url to slug tags, instead of object PK

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -22,6 +22,7 @@ from products.views import (
     product_list_view,
     ProductDetailView,
     product_detail_view,
+    ProductDetailSlugView,
     ProductFeaturedListView,
     ProductFeaturedDetailView
 )
@@ -38,8 +39,9 @@ urlpatterns = [
     url(r'^featured/$', ProductFeaturedListView.as_view()),
     url(r'^featured/(?P<pk>\d+)$', ProductFeaturedDetailView.as_view()),
     url(r'^products-fbv/$', product_list_view),
-    url(r'^products/(?P<pk>\d+)$', ProductDetailView.as_view()),
+    # url(r'^products/(?P<pk>\d+)$', ProductDetailView.as_view()),
     url(r'^products-fbv/(?P<pk>\d+)$', product_detail_view),
+    url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
 
--- src/products/admin.py
+++ src/products/admin.py
@@ -9,4 +9,4 @@ class ProductAdmin(admin.ModelAdmin):
     class Meta:
         model = Product
 
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
