d33969ac78978075f483175659e594f0839c089e
robdunn220 <robertdunn220@gmail.com>
2018-12-05 08:12:32 -0500

    Working with integrating mobile bootstrap styling

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url, include
-
+from django.views.generic import TemplateView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^contact/$', contact_page, name='contact'),
     url(r'^login/$', login_page, name='login'),
     url(r'^register/$', register_page, name='register'),
+    url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include(("products.urls"), namespace='products')),
     url(r'^admin/', admin.site.urls),
 ]
